099c9718e25e994df8380e035c21d44e2a0449dc
QuentinHsu <xuquentinyang@gmail.com>
2023-09-10 22:58:04 +0800

    add basic/function.go

---
+++ basic/function.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func fool(a string, b int) int {
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+	c := 100
+	return c
+}
+
+func main() {
+	c := fool("abc", 345)
+	fmt.Println("c = ", c)
+}
